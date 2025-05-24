68e418d33fd38b11966529c68b18ebf4469d0dbd
Abhishek <abhishek.mehrotra27@gmail.com>
2025-05-24 23:23:50 +0530

    Fetched the data from the API and display result on the FE

--- SQLRunner/views.py
+++ SQLRunner/views.py
@@ -1,29 +1,35 @@
 from django.http import HttpResponse
 from django.http import JsonResponse
+from django.views.decorators.http import require_POST
+from django.views.decorators.csrf import csrf_exempt
 from django.db import connection
 import json
 
 def home(request):
     return HttpResponse("Welcome to SQL Runner! Use /run_query/ to execute your SQL queries.")
 
+@require_POST
+@csrf_exempt
 def run_query(request):
-    if request.method != 'GET':
-        return JsonResponse({'error': 'Only GET method is allowed'}, status=405)
+    if not request.body:
+        return JsonResponse({'Request body cannot be empty'}, status=400)
+    
     data = json.loads(request.body.decode('utf-8'))
     query = data.get('query')
-    if not query:
-        return JsonResponse({'error': 'No query provided'}, status=400)
     try:
         with connection.cursor() as cursor:
             cursor.execute(query)
             query_type = query.strip().split(" ",1)[0].lower()
+
             if query_type in ['select']:
                 columns = [col[0] for col in cursor.description]
                 results = [dict(zip(columns, row)) for row in cursor.fetchall()]
                 return JsonResponse({'results': results}, status=200)
+            
             elif query_type in ['insert', 'update', 'delete', 'create', 'alter', 'drop']:
                 results = {'message': f'{query_type.capitalize()} executed successfully'}
                 return JsonResponse(results, status=200)
+            
             else:
                 return JsonResponse({'error': 'Unsupported query type'}, status=400)
     except Exception as e:
